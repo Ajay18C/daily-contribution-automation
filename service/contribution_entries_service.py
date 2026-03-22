7ccea09a0150e0c6e421fd90375bec14033be436
Ajay18C <trojanajay4309@gmail.com>
2026-03-22 10:45:54 +0530

    feat: Add `ContributionEntriesService` to manage skilltap contribution entries.

--- service/contribution_entries_service.py
+++ service/contribution_entries_service.py
@@ -6,18 +6,9 @@ class ContributionEntriesService:
     
     async def get_contribution_entries(self):
         return await self.contribution_entries_repository.find({})
-    
-    async def get_contribution_entry(self, id: str):
-        return await self.contribution_entries_repository.find_one({"_id": id})
 
     async def get_entry_by_date(self, date_str: str):
         return await self.contribution_entries_repository.find_one({"time_log_date": date_str})
     
     async def create_contribution_entry(self, contribution_entry: dict):
         return await self.contribution_entries_repository.insert_one(contribution_entry)
-    
-    async def update_contribution_entry(self, id: str, contribution_entry: dict):
-        return await self.contribution_entries_repository.update_one({"_id": id}, {"$set": contribution_entry})
-    
-    async def delete_contribution_entry(self, id: str):
-        return await self.contribution_entries_repository.delete_one({"_id": id})
